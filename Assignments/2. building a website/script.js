7ebe0aedcb22d436750383769b81504df3c3e3c1
Avishka <78788710+avishka777dilshan@users.noreply.github.com>
2023-01-23 21:19:52 +0530

    Add files via upload

---
+++ Assignments/2. building a website/script.js
@@ -0,0 +1,15 @@
+function onMouse(){
+    document.getElementById("color").style.color="red";
+}
+function outMouse(){
+    document.getElementById("color").style.color="black";
+}
+
+function bigImg(x){
+    x.style.height = "800px";
+    x.style.width = "800px";
+}
+function normalImg(x){
+    x.style.height = "640px";
+    x.style.width = "640px";
+}
